2dba191edd4e8b33bd1d7a66416735b968ef3d07
rhasanrakib <hasan@sharetrip.net>
2021-11-03 12:42:22 +0600

    clean code rmq strategy

--- src/strategy.ts
+++ src/strategy.ts
@@ -20,9 +20,6 @@ export class RmqStrategy extends ServerRMQ implements CustomTransportStrategy {
     message: Record<string, any>,
     channel: any,
   ): Promise<void> {
-    if (isNil(message)) {
-      return;
-    }
     const { content, properties } = message;
 
     let rawMessage: any;
@@ -39,47 +36,15 @@ export class RmqStrategy extends ServerRMQ implements CustomTransportStrategy {
           ...{ body: other },
         };
       }
-      console.log(rawMessage);
     } catch (error) {
       errorMessage = {
         ...{ error: error.message },
         ...{ body: content.toString('utf-8') },
       };
       rawMessage = JSON.parse(JSON.stringify(errorMessage));
-      console.log(rawMessage);
     }
-
-    const packet = await this.deserializer.deserialize(rawMessage);
-    const pattern = isString(packet.pattern)
-      ? packet.pattern
-      : JSON.stringify(packet.pattern);
-
-    const rmqContext = new RmqContext([message, channel, pattern]);
-    if (isUndefined((packet as IncomingRequest).id)) {
-      return this.handleEvent(pattern, packet, rmqContext);
-    }
-    const handler = this.getHandlerByPattern(pattern);
-
-    if (!handler) {
-      const status = 'error';
-      const noHandlerPacket = {
-        id: (packet as IncomingRequest).id,
-        err: NO_MESSAGE_HANDLER,
-        status,
-      };
-      return this.sendMessage(
-        noHandlerPacket,
-        properties.replyTo,
-        properties.correlationId,
-      );
-    }
-    const response$ = this.transformToObservable(
-      await handler(packet.data, rmqContext),
-    ) as Observable<any>;
-
-    const publish = <T>(data: T) =>
-      this.sendMessage(data, properties.replyTo, properties.correlationId);
-
-    response$ && this.send(response$, publish);
+    message.content = JSON.stringify(rawMessage);
+    message.properties = properties;
+    return super.handleMessage(message, channel);
   }
 }
